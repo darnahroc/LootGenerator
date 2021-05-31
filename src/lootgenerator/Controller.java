022095cc6fc11f91f7a95e5a1c838691cc9b760b
darnahroc <jhmunlimited.com@gmail.com>
2021-05-31 17:35:01 -0500

    update

--- src/lootgenerator/Controller.java
+++ src/lootgenerator/Controller.java
@@ -1,25 +1,31 @@
 package lootgenerator;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 
-@SuppressWarnings({"unused", "rawtypes"})
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@SuppressWarnings({"rawtypes", "unused"})
 public class Controller {
 
+    @FXML
     public Button loot_btn;
     public Button sav_txt_btn;
     public ListView loot_lst;
-    public ChoiceBox<String> loot_settings = new ChoiceBox<>();
-      String a = "3.5";
-      String b = "Pathfinder";
-      String c = "5e";
-      String d = "Modern";
-      String e = "Future";
-      String f = "All";
+    @FXML
+    private final ChoiceBox<String> loot_settings;
 
-
+    {
+        loot_settings = new ChoiceBox<>();
+        loot_settings.getItems().addAll("All", "3.5", "Modern", "Future", "Pathfinder", "5e");
+        loot_settings.getItems().add("test");
+        loot_settings.setValue("All");
+    }
 
 
     public void handleButtonAction(ActionEvent actionEvent) {
@@ -28,4 +34,35 @@ public class Controller {
 
     public void fileChooser(ActionEvent actionEvent) {
     }
-}
+
+        public static class randomgen {
+
+            // Drive Function
+            public static void main(String[] args)
+            {
+
+                // create a list of Integer type
+                List<Integer> list = new ArrayList<>();
+                // add 5 element in ArrayList
+                list.add(10);
+                list.add(20);
+                list.add(30);
+                list.add(40);
+                list.add(50);
+
+                randomgen obj = new randomgen();
+
+                // take a random element from list and print them
+                System.out.println(obj.getRandomElement(list));
+            }
+
+            // Function select an element base on index
+            // and return an element
+            public int getRandomElement(List<Integer> list)
+            {
+                Random rand = new Random();
+                return list.get(rand.nextInt(list.size()));
+            }
+        }
+    }
+
--- src/lootgenerator/Main.java
+++ src/lootgenerator/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LootGenerator.fxml")));
         primaryStage.setTitle("Loot Generator");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 550, 750));
         primaryStage.show();
     }
 
